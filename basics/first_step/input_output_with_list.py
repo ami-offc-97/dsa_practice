0e23ebbb6cbbdf8ce9362e4e7f7b703a08cdb1e0
Amit Kumar <amit_kumar@shtlp0210.local>
2024-03-04 11:09:50 +0530

    use of list for input & output

---
+++ basics/first_step/input_output_with_list.py
@@ -0,0 +1,12 @@
+#print("Enter the list elements ")
+#List_num = list(map(int, input().split()))
+#print("List elemnts are : ")
+#print(List_num)
+
+#alternative way
+n = int(input("Enter the number of elements in list : "))
+List = list()
+print("enter the list elements")
+for i in range(0,n):
+    List.append(int(input()))
+print(List)    
